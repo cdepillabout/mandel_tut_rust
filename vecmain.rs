bf63ac823f3398d581f9625febd8e2f0ed496c2a
(cdep)illabout <cdep.illabout@gmail.com>
2019-08-03 16:42:18 +0900

    update

--- vecmain.rs
+++ vecmain.rs
@@ -1,7 +1,7 @@
 extern crate image;
-extern crate num_complex;
 extern crate num;
- 
+extern crate num_complex;
+
 use std::ops::Add;
 use std::ops::Div;
 use std::ops::Mul;
@@ -9,8 +9,8 @@ use std::ops::Sub;
 
 // use std::fs::File;
 // use std::path::Path;
-use num::Float;
 use num::Complex;
+use num::Float;
 // use num_complex::Complex;
 use image::ImageBuffer;
 use image::Luma;
@@ -22,7 +22,7 @@ struct Bound<T> {
 
 // fn calc_scale<T, U>(lala: &Bound<T>, img_size: U) -> T
 //     where T: Sub + Div,
-//           U: Clone + Into<T> 
+//           U: Clone + Into<T>
 
 // {
 //   // (self.max - self.min)  / T::from(img_size)
@@ -32,14 +32,15 @@ struct Bound<T> {
 // }
 
 impl<T> Bound<T>
-where T: Sub<Output = T> + Div<Output = T> + Copy
+where
+    T: Sub<Output = T> + Div<Output = T> + Copy,
 {
     fn scale<U: Into<T>>(&self, img_size: U) -> T {
         (self.max - self.min) / img_size.into()
     }
 }
- 
-fn main(){
+
+fn main() {
     let max_iterations = 100u16;
     let img_size = 800u16;
 
@@ -52,23 +53,31 @@ fn main(){
         min: -1.5f32,
         max: 1.5f32,
     };
-  
+
     // print_buf(foo);
 
     dbg!(cx_bound.scale(img_size));
     dbg!(cy_bound.scale(img_size));
 
-    draw_mandel(max_iterations, img_size, &cx_bound, &cy_bound);
+    draw_mandel(max_iterations, img_size, 2.0_f32, &cx_bound, &cy_bound);
 }
 
-fn draw_mandel<T>(max_iterations:u16, img_size:u16, norm_breakpoint: T, cx_bound: &Bound<T>, cy_bound: &Bound<T>) -> ImageBuffer<Luma<u8>, Vec<u8>>
-    where T: Float + Copy + From<u16>
+fn draw_mandel<T>(
+    max_iterations: u16,
+    img_size: u16,
+    norm_breakpoint: T,
+    cx_bound: &Bound<T>,
+    cy_bound: &Bound<T>,
+) -> ImageBuffer<Luma<u8>, Vec<u8>>
+where
+    T: Float + Copy + From<u16>,
 {
-    let mut imgbuf : ImageBuffer<Luma<u8>, Vec<u8>> = image::ImageBuffer::new(img_size.into(), img_size.into());
+    let mut imgbuf: ImageBuffer<Luma<u8>, Vec<u8>> =
+        image::ImageBuffer::new(img_size.into(), img_size.into());
 
     for (x, y, pixel) in imgbuf.enumerate_pixels_mut() {
-        let x_T : T = (x as u16).into();
-        let y_T : T = (y as u16).into();
+        let x_T: T = (x as u16).into();
+        let y_T: T = (y as u16).into();
 
         let cx = cx_bound.min + x_T * cx_bound.scale(img_size);
         let cy = cy_bound.min + y_T * cy_bound.scale(img_size);
@@ -78,7 +87,7 @@ fn draw_mandel<T>(max_iterations:u16, img_size:u16, norm_breakpoint: T, cx_bound
 
         let mut i = 0;
         for t in 0..max_iterations {
-            if z.norm() > 2.0 {
+            if z.norm() > norm_breakpoint {
                 break;
             }
             // z = z * z + c;
